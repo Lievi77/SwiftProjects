4d8df3a7389ebfac5afd4c475440cce247d1846e
Lievi77 <lev.guzman@ideen.mx>
2021-01-30 09:53:57 -0500

    Initialized I am Rich Project

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Lev C. Guzman Aparicio on 2021-01-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
